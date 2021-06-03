71102c9b7c62366b216154cb074a521c5e5624b1
JeHwan Yoo <yoojehwan1875@gmail.com>
2021-06-03 23:01:11 +0900

    vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import lottos from '@/store/lotto-crawler/results/lottos-1-965.json'
 
 Vue.use(Vuex)
 
+interface LottoData {
+  no: number
+  numbers: number[]
+}
+
 export default new Vuex.Store({
-  state: {},
+  state: {
+    lottos,
+  },
+  getters: {
+    lottos(state): LottoData[] {
+      return state.lottos
+    },
+  },
   mutations: {},
   actions: {},
-  modules: {},
 })
