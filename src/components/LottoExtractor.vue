a74a8ae18e47fe29d2c47849c62ecba440b85dbf
JeHwan Yoo <yoojehwan1875@gmail.com>
2021-06-05 01:12:46 +0900

    extract, copy, save

--- src/components/LottoExtractor.vue
+++ src/components/LottoExtractor.vue
@@ -1,24 +1,36 @@
 <template>
-  <v-sheet class="pa-2" elevation="3">
+  <v-sheet class="lotto-extractor pa-2" elevation="3">
     <v-container>
       <v-row>
         <v-col>
-          <v-btn x-large color="success">추출하기</v-btn>
+          <v-btn x-large color="green accent-3">추출하기</v-btn>
+        </v-col>
+        <v-col v-show="extracted.length > 0">
+          <v-btn x-large color="lime accent-1" @click="copy">복사하기</v-btn>
+        </v-col>
+        <v-col v-show="extracted.length > 0">
+          <v-btn x-large color="amber accent-3" @click="save">저장하기</v-btn>
         </v-col>
       </v-row>
       <v-row v-for="(numbers, index) in extracted" :key="index">
         <v-col>
-          <v-avatar
-            class="mx-1"
-            v-for="(number, index) in numbers"
-            :key="index"
-            :color="selectColor(number)"
-          >
-            <span>{{ number }}</span>
-          </v-avatar>
+          <v-sheet class="pa-2" color="indigo lighten-5">
+            <v-avatar
+              class="mx-1"
+              v-for="(number, index) in numbers"
+              :key="index"
+              :color="selectColor(number)"
+              :size="ballSize"
+            >
+              <span class="font-weight-bold">{{ number }}</span>
+            </v-avatar>
+          </v-sheet>
         </v-col>
       </v-row>
     </v-container>
+    <v-snackbar :timeout="1500" v-model="snackbar">
+      {{ snackbarText }}
+    </v-snackbar>
   </v-sheet>
 </template>
 
@@ -31,6 +43,16 @@ export default class LottoExtractor extends Vue {
   @Getter('extracted') extracted!: number[][]
   @Mutation('clearExtacted') clearExtracted!: () => void
   @Mutation('pushExtracted') pushExtracted!: (numbers: number[]) => void
+  snackbar = false
+  snackbarText = ''
+
+  get ballSize(): number {
+    return this.$vuetify.breakpoint.smAndDown ? 30 : 40
+  }
+
+  get clipboardText(): string {
+    return this.extracted.map(numbers => numbers.toString()).join('\n')
+  }
 
   selectColor(n: number): string {
     if (n < 10) {
@@ -46,11 +68,49 @@ export default class LottoExtractor extends Vue {
     }
   }
 
+  copy(): void {
+    this.$copyText(this.clipboardText)
+      .then(() => {
+        this.snackbar = true
+        this.snackbarText = '클립보드에 복사되었습니다!'
+      })
+      .catch(() => {
+        this.snackbar = true
+        this.snackbarText = '에러가 발생했습니다! 다시 시도해주세요.'
+      })
+  }
+
+  save(): void {
+    const blob = new Blob([this.clipboardText], { type: 'text/csv' })
+    const filename = `로또번호-${Date.now()}.csv`
+    // for Internet Explorer
+    if (/Trident\/|Edge\//.test(window.navigator.userAgent)) {
+      window.navigator.msSaveOrOpenBlob(blob, filename)
+    }
+    // for Firefox, Chrome, Opera
+    else {
+      var fileLink = document.createElement('a')
+      fileLink.download = filename
+      fileLink.href = window.URL.createObjectURL(blob)
+      document.body.appendChild(fileLink)
+      fileLink.click()
+      fileLink.remove()
+    }
+  }
+
   mounted(): void {
     this.clearExtracted()
-    this.pushExtracted([1, 3, 10, 15, 20, 25])
-    this.pushExtracted([1, 2, 36, 42, 44, 45])
-    console.log(this.extracted)
+    this.pushExtracted([1, 2, 3, 4, 5, 6])
+    this.pushExtracted([1, 2, 3, 4, 5, 6])
+    this.pushExtracted([1, 2, 3, 4, 5, 6])
+    this.pushExtracted([1, 2, 3, 4, 5, 6])
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.lotto-extractor {
+  max-height: 400px;
+  overflow-y: auto;
+}
+</style>
