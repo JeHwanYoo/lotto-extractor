2f1e57b6886ef8b1d8d878a1778941ed99ebd8bd
JeHwan Yoo <yoojehwan1875@gmail.com>
2021-06-05 17:17:59 +0900

    meta tag

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,10 @@ module.exports = {
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].title = '행운의 로또 추출기'
+      args[0].description = '당신에게 행운의 로또 번호를 알려줍니다.'
+      args[0].imageURL =
+        'https://jehwanyoo.net/lotto/android-chrome-192x192.png'
+      args[0].siteURL = 'https://jehwanyoo.net/lotto/'
       return args
     })
   },
